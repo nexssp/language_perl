9a35b1142d23653ed2284e6eed79e2fecd17f124
Marcin Polak <mapoart@gmail.com>
2020-02-15 12:29:39 +0100

    Added founders, developers and year ..

--- perl.win32.nexss.config.js
+++ perl.win32.nexss.config.js
@@ -3,6 +3,9 @@ languageConfig.title = "Perl";
 languageConfig.description =
   "Perl 5 is a highly capable, feature-rich programming language with over 30 years of development.";
 languageConfig.url = "https://www.perl.org/";
+languageConfig.founders = ["Larry Wall"];
+languageConfig.developers = [""];
+languageConfig.years = ["1987"];
 languageConfig.extensions = [".pl"];
 languageConfig.builders = {}; // Check cpp or python to fill in example
 languageConfig.compilers = {
