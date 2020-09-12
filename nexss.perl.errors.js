a667976298fd4f8d53530d84e52b91ebff853916
Marcin Polak <mapoart@gmail.com>
2020-09-12 20:04:37 +0200

    Added cpan/cpan json for Oracle Linux

--- nexss.perl.errors.js
+++ nexss.perl.errors.js
@@ -1,6 +1,14 @@
 // More about error handling in Nexss Programmer can be found:
 // https://github.com/nexssp/cli/wiki/Errors-Solutions
+const {
+  replaceCommandByDist,
+} = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
+
 module.exports = {
   "Undefined subroutine &main::(?<function>.*?) called at myfile.pl line (?<line>.*?),":
     "Function not found '<function>' at line <line>. You may need to look at: https://perldoc.perl.org/index-functions.html",
+  "Can't locate CPAN.pm": replaceCommandByDist("apt-get install perl-CPAN"),
+  "Can't locate JSON/PP.pm":
+    replaceCommandByDist("apt-get install -y perl-CPAN") +
+    " PERL_MM_USE_DEFAULT=1 cpan -i CPAN && cpan JSON",
 };
--- perl.linux.nexss.config.js
+++ perl.linux.nexss.config.js
@@ -1,11 +1,32 @@
 let languageConfig = Object.assign({}, require("./perl.win32.nexss.config"));
 languageConfig.compilers = {
   perl5: {
-    install: "apt install -y perl",
+    install:
+      "PERL_MM_USE_DEFAULT=1 apt install perl && apt install perl-CPAN && PERL_MM_USE_DEFAULT=1 cpan -i CPAN && cpan JSON",
     command: "perl",
     args: "<file>",
     help: ``,
   },
 };
+
+const {
+  replaceCommandByDist,
+  dist,
+} = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
+
+const distName = dist();
+languageConfig.dist = distName;
+
+// TODO: Later to cleanup this config file !!
+switch (distName) {
+  // case "Arch Linux":
+  //   languageConfig.compilers.perl5.install = `${sudo}pacman -Sy --noconfirm perl`;
+  //   break;
+  default:
+    languageConfig.compilers.perl5.install = replaceCommandByDist(
+      languageConfig.compilers.perl5.install
+    );
+    break;
+}
 
 module.exports = languageConfig;
