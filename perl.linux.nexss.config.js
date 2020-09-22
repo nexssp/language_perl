15c4a9510d1456e89f52ee1d0de2c396387d42a6
Marcin Polak <mapoart@gmail.com>
2020-09-22 18:15:13 +0200

    latest progress..

--- perl.linux.nexss.config.js
+++ perl.linux.nexss.config.js
@@ -1,28 +1,30 @@
 let languageConfig = Object.assign({}, require("./perl.win32.nexss.config"));
-let sudo = "sudo ";
-if (process.getuid && process.getuid() === 0) {
-  sudo = "";
-}
+const os = require("@nexssp/os");
+const sudo = os.sudo();
 
 languageConfig.compilers = {
   perl5: {
-    install: `${sudo}PERL_MM_USE_DEFAULT=1 apt install perl make && apt install perl-CPAN && PERL_MM_USE_DEFAULT=1 cpan && cpan JSON::PP`,
+    install: `${sudo}PERL_MM_USE_DEFAULT=1 
+apt install perl make`,
     command: "perl",
     args: "<file>",
     help: ``,
   },
 };
 
-const {
-  replaceCommandByDist,
-  dist,
-} = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
-
-const distName = dist();
+const distName = os.name();
 languageConfig.dist = distName;
 
 // TODO: Later to cleanup this config file !!
 switch (distName) {
+  case "Oracle Linux Server":
+    languageConfig.compilers.perl5.install =
+      `${sudo}apt-get install -y gcc gcc-c++ kernel-devel make
+` + languageConfig.compilers.perl5.install;
+    languageConfig.compilers.perl5.install = os.replacePMByDistro(
+      languageConfig.compilers.perl5.install
+    );
+    break;
   case "Alpine Linux": // 3.12
     languageConfig.compilers.perl5.install = `${sudo}apk add ca-certificates wget
 apk add perl make perl-app-cpanminus
@@ -33,7 +35,7 @@ cpanm JSON::PP`;
   case "openSUSE Tumbleweed":
     languageConfig.compilers.perl5.install = `${sudo}zypper -n perl make perl-App-cpanminus  && PERL_MM_USE_DEFAULT=1 cpan && cpan JSON::PP`;
   default:
-    languageConfig.compilers.perl5.install = replaceCommandByDist(
+    languageConfig.compilers.perl5.install = os.replacePMByDistro(
       languageConfig.compilers.perl5.install
     );
     break;
