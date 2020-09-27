51b0400f40e55b6e98a2d4ed82999ec033763f53
Marcin Polak <mapoart@gmail.com>
2020-09-27 18:53:37 +0200

    Added rapidjson for templates.

---
+++ templates/default.pl.js
@@ -0,0 +1,42 @@
+// additional operations for templates like copy extra libraries.
+
+function getInstaller() {
+  const os = require("@nexssp/os"); // this is included in main nexss programmer cli: @nexssp/cli
+
+  switch (process.platform) {
+    case "win32":
+      return "";
+    case "linux":
+      switch (os.name()) {
+        // case os.distros.ARCH:
+        //   return replaceCommandByDist("pacman -S --noconfirm -y rapidjson");
+        // case os.distros.ORACLE:
+        //   return replaceCommandByDist(
+        //     "yum install -y perl-CPAN && PERL_MM_USE_DEFAULT=1 cpan -i CPAN && perl -MCPAN -e 'install JSON::PP'"
+        //   );
+        // case os.distros.FEDORA:
+        //   return replaceCommandByDist("dnf install -y rapidjson-devel");
+        default:
+          return replaceCommandByDist(`PERL_MM_USE_DEFAULT=1
+apt install perl-CPAN
+cpan JSON::PP`);
+      }
+
+    case "darwin":
+      return "brew install rapidjson-dev";
+    default:
+      console.error(`${process.platform} is not implemented.`);
+      break;
+  }
+}
+
+const config = {
+  files: [],
+  commands: [getInstaller()],
+  repos: ["https://github.com/Tencent/rapidjson.git"],
+  descriptions: [
+    "Rapid json has been installed. License information here: https://github.com/Tencent/rapidjson.git",
+  ],
+};
+
+module.exports = config;
