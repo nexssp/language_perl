d5e80c1ada73f4fd3f2029d0857145a5d8c3a917
Marcin Polak <mapoart@gmail.com>
2021-04-27 21:25:06 +0200

    remove dependency of @nexssp/os. #36

--- perl.linux.nexss.config.js
+++ perl.linux.nexss.config.js
@@ -1,8 +1,7 @@
 let languageConfig = Object.assign({}, require("./perl.win32.nexss.config"));
 // const os = require("@nexssp/os")
 // Load os from Nexss CLI path (it can be changed if needed)
-const os = require(`${process.env.NEXSS_SRC_PATH}/node_modules/@nexssp/os/`);
-const sudo = os.sudo();
+const sudo = process.sudo;
 
 languageConfig.compilers = {
   perl5: {
@@ -13,30 +12,30 @@ languageConfig.compilers = {
   },
 };
 
-const distName = os.name();
+const distName = process.distro;
 languageConfig.dist = distName;
 
 // TODO: Later to cleanup this config file !!
 switch (distName) {
-  case "Oracle Linux Server":
+  case process.distros.ORACLE:
     languageConfig.compilers.perl5.install =
       `${sudo}apt-get install -y gcc gcc-c++ kernel-devel make
 ` + languageConfig.compilers.perl5.install;
-    languageConfig.compilers.perl5.install = os.replacePMByDistro(
+    languageConfig.compilers.perl5.install = process.replacePMByDistro(
       languageConfig.compilers.perl5.install
     );
     break;
-  case "Alpine Linux": // 3.12
+  case process.distros.ALPINE: // 3.12
     languageConfig.compilers.perl5.install = `${sudo}apk add ca-certificates wget
 apk add perl make perl-app-cpanminus
 PERL_MM_USE_DEFAULT=1 cpanm
 cpanm JSON::PP`; // TODO: move json:pp to /templates/
     break;
-  case "openSUSE Leap":
-  case "openSUSE Tumbleweed":
+  case process.distros.SUSE_LEAP:
+  case process.distros.SUSE_TUMBLEWEED:
     languageConfig.compilers.perl5.install = `${sudo}zypper -n perl make perl-App-cpanminus  && PERL_MM_USE_DEFAULT=1 cpan && cpan JSON::PP`;
   default:
-    languageConfig.compilers.perl5.install = os.replacePMByDistro(
+    languageConfig.compilers.perl5.install = process.replacePMByDistro(
       languageConfig.compilers.perl5.install
     );
     break;
